c508706de93ddba52e2e6c1f3d78780b611a251a
Matthias Monschein <m4ttm00ny@gmail.com>
2022-12-09 17:35:54 +0100

    :construction: extended platform

--- platforms/platform/__init__.py
+++ platforms/platform/__init__.py
@@ -3,29 +3,8 @@ Dummy code for how a platform should
 be implemented
 """
 
+from .platform import Platform
 from typing import *
 
-class Platform(object):
-    """
-    Represents a social-media platform
-    """
-    
-    def __init__(self, name: str, desc: str):
-        """
-        Creates a new platform
-
-        Args:
-            name (str): The name of the platform
-            desc (str): A description of the platform
-        """
-        self.name: str = name
-        self.desc: str = desc
-
-    def __str__(self) -> str:
-        """
-        Returns the string representation of the platform
-
-        Returns:
-            str: The string representation of the platform
-        """
-        return self.name
+def init() -> Platform:
+    return Platform()
---
+++ platforms/platform/platform.py
@@ -0,0 +1,96 @@
+"""
+Dummy code for the platform class.
+"""
+
+from platforms.platform.info import Info
+from typing import *
+
+class Platform(object):
+    """
+    Represents a social-media platform
+    """
+    
+    def __init__(self, name: str, desc: str):
+        """
+        Creates a new platform
+
+        Args:
+            name (str): The name of the platform
+            desc (str): A description of the platform
+        """
+        self.name: str = name
+        self.desc: str = desc
+
+    @classmethod
+    def get_user_description(cls, username: str) -> Info:
+        """
+        Gets the description of a user
+
+        Args:
+            username (str): The username of the user
+
+        Returns:
+            Info: The description of the user
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def get_user_profile_pic(cls, username: str) -> Info:
+        """
+        Gets the profile picture of a user
+
+        Args:
+            username (str): The username of the user
+
+        Returns:
+            Info: The path to the profile picture of the user
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def get_user_followers(cls, username: str) -> Info:
+        """
+        Gets the followers of a user
+
+        Args:
+            username (str): The username of the user
+
+        Returns:
+            Info: The followers of the user
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def get_user_following(cls, username: str) -> Info:
+        """
+        Gets the following of a user
+
+        Args:
+            username (str): The username of the user
+
+        Returns:
+            Info: The following of the user
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def get_user_posts(cls, username: str) -> Info:
+        """
+        Gets the posts of a user
+
+        Args:
+            username (str): The username of the user
+
+        Returns:
+            Info: The posts of the user
+        """
+        raise NotImplementedError()
+
+    def __str__(self) -> str:
+        """
+        Returns the string representation of the platform
+
+        Returns:
+            str: The string representation of the platform
+        """
+        return self.name
