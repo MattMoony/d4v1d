b3f9c0da162754f25b58a1bb9512a1c35482094e
Matthias Monschein <m4ttm00ny@gmail.com>
2023-03-25 10:38:55 +0100

    :children_crossing: prettier `show users`

--- d4v1d/cmd/show/users.py
+++ d4v1d/cmd/show/users.py
@@ -5,9 +5,12 @@ cached locally for the used platform.
 
 from typing import List
 
-from d4v1d.utils import io
 from d4v1d.platforms.platform.cmd.clisessionstate import CLISessionState
 from d4v1d.platforms.platform.cmd.cmd import Command
+from d4v1d.platforms.platform.info import Info
+from d4v1d.platforms.platform.user import User
+from d4v1d.utils import io
+
 
 class ShowUsers(Command):
     """
@@ -43,6 +46,10 @@ class ShowUsers(Command):
             argv (List[str]): The arguments passed to the command.
             state (CLISessionState): The current session state.
         """
+        users: List[Info[User]] = state.platform.users()
+        if not users:
+            io.w('No users have been cached for this platform yet!')
+            return
         io.l(f'All users, whose info have been cached for "{state.platform.name}":')
-        for i in state.platform.users():
+        for i in users:
             io.n(f'[bold]{i.value.username}[/bold] @ [italic dim]"{i.date.isoformat()}"[/italic dim]')
