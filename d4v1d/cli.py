944c5b41e21b2896d450a15b48b17d475be0499f
Matthias Monschein <monschein.matthias@gmail.com>
2023-08-13 18:01:17 +0200

    :sparkles: introduced mimikatz-like convenience feature for passing commands as arguments

--- d4v1d/cli.py
+++ d4v1d/cli.py
@@ -3,6 +3,8 @@ The CLI module of the program - contains
 the main entry point of the program
 """
 
+from argparse import ArgumentParser
+
 from d4v1d import cmd, utils
 
 
@@ -10,8 +12,12 @@ def main():
     """
     The main entry point of the program
     """
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument('cmds', nargs='*')
+    args = parser.parse_args()
+
     print(utils.title('d4v1d'))
-    cmd.start()
+    cmd.start(args.cmds)
 
 if __name__ == '__main__':
     main()
--- d4v1d/cmd/__init__.py
+++ d4v1d/cmd/__init__.py
@@ -3,13 +3,20 @@ Module responsible for receiving and processing
 commands from the user.
 """
 
+from typing import List, Optional
+
+from d4v1d.utils import io
 from d4v1d.cmd._helper import CMDS
 from d4v1d.cmd._helper.session import CmdSession
 
 
-def start() -> None:
+def start(cmds: Optional[List[str]]) -> None:
     """
     Starts handling user input.
     """
     session: CmdSession = CmdSession(CMDS)
-    session.handle_forever()
+    if not cmds:
+        session.handle_forever()
+    for c in cmds:
+        io.l(f'[bold]# {c}[/bold]')
+        session.handle(c)
